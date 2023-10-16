40050124509105a277d77f33226409f2c0c026e8
Dovy <skurdelis.dovydas@gmail.com>
2023-10-16 16:36:58 +0100

    New activity button

--- app/src/main/java/com/example/counterredux/MainActivity.kt
+++ app/src/main/java/com/example/counterredux/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.counterredux
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+
+        val matchActivityButton = findViewById<Button>(R.id.Button_Match)
+        matchActivityButton.setOnClickListener {
+            val Intent = Intent(this,MatchActivity::class.java)
+            startActivity(Intent)
+        }
+
     }
 }
