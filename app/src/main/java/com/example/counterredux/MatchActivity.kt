56e3bde28bf1e18b58da17416c8af07ea8c9d12f
Dovy <skurdelis.dovydas@gmail.com>
2023-10-17 14:48:13 +0100

    Return Button navigation between activities

--- app/src/main/java/com/example/counterredux/MatchActivity.kt
+++ app/src/main/java/com/example/counterredux/MatchActivity.kt
@@ -1,11 +1,22 @@
 package com.example.counterredux
 
+import android.content.Intent
+import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 
 class MatchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_match)
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE)
+
+        val returnActivityButton = findViewById<ImageButton>(R.id.Button_Return)
+        returnActivityButton.setOnClickListener {
+            val Intent = Intent(this,MainActivity::class.java)
+            startActivity(Intent)
+        }
     }
 }
